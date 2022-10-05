55dd22db42d6ae12a08c990d4d14f62930d47436
Aloxan12 <kvakvaaak@mail.ru>
2022-10-05 16:04:09 +0300

    input

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputComponent implements OnInit {
   todoTitle: string = '';
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
